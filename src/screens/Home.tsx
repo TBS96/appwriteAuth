762d593c0552160d9a00916a617edf83b515d3f2
TBS96 <9tbs6@proton.me>
2024-08-25 16:42:39 +0530

    Home Screen: import { FAB } from '@rneui/themed' , import Snackbar from 'react-native-snackbar' , import { AppwriteContext } from '../appWrite/AppwriteContext' ; render userData
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,111 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
 
-const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+// RN styling elements
+import { FAB } from '@rneui/themed'
+
+// Snackbar
+import Snackbar from 'react-native-snackbar'
+
+// Context API
+import { AppwriteContext } from '../appwrite/AppwriteContext'
+
+type UserObj = {
+  name: String;
+  email: String;
 }
 
-export default Home
 
-const styles = StyleSheet.create({})
+const Home = () => {
+
+  const [userData, setUserData] = useState<UserObj>();
+
+  const {appwrite, setIsLoggedIn} = useContext(AppwriteContext)
+
+  const handleLogout = () => {
+    appwrite.logout()
+    .then(() => {
+      setIsLoggedIn(false);
+      Snackbar.show({
+        text: 'Logout Successful!',
+        duration: Snackbar.LENGTH_SHORT
+      })
+    })
+  }
+
+  useEffect(() => {
+    appwrite.getCurrentUser()
+    .then(response => {
+      if(response)
+      {
+        const user: UserObj ={
+          name: response.name,
+          email: response.email
+        }
+        setUserData(user)
+      }
+    })
+  }, [appwrite])
+  
+
+  return (
+    <SafeAreaView style={styles.container}>
+        <View style={styles.welcomeContainer}>
+          <Image
+            source={{
+              uri: 'https://media.istockphoto.com/id/1332104709/photo/shot-of-a-young-male-engineer-using-his-laptop-in-a-server-room.jpg?s=1024x1024&w=is&k=20&c=MAhJL---c2n8Ei5oWHADXb-5rVtjvHtAsYzxXfpVUe4=',
+              width: 400,
+              height: 300,
+              cache: 'default',
+            }}
+            resizeMode="contain"
+          />
+          <Text style={styles.message}>
+            Build Fast. Scale Big. All in One Place.
+          </Text>
+          {userData && (
+            <View style={styles.userContainer}>
+              <Text style={styles.userDetails}>Name: {userData.name}</Text>
+              <Text style={styles.userDetails}>Email: {userData.email}</Text>
+            </View>
+          )}
+        </View>
+        <FAB
+          placement="right"
+          color="#f02e65"
+          size="large"
+          title="Logout"
+          icon={{name: 'logout', color: '#FFFFFF'}}
+          onPress={handleLogout}
+        />
+      </SafeAreaView>
+  )
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0B0D32',
+  },
+  welcomeContainer: {
+    padding: 12,
+
+    flex: 1,
+    alignItems: 'center',
+  },
+  message: {
+    fontSize: 26,
+    fontWeight: '500',
+    color: '#FFFFFF',
+  },
+  userContainer: {
+    marginTop: 24,
+  },
+  userDetails: {
+    fontSize: 20,
+    color: '#FFFFFF',
+  },
+});
+
+export default Home
